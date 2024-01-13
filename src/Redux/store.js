1f986c85dff9aaedc6d8dc4beae13d9abaf871db
diptanshumkm <diptanshu.uae@gmail.com>
2024-01-14 00:25:50 +0530

    first commit

---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CounterSlice from "./CounterSlice";
+
+
+
+export const store = configureStore(
+    {
+        reducer : {
+            counter: CounterSlice
+        }
+    }
+)
